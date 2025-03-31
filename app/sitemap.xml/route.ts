c959c4c2a1a0decba1e16a9db761633ca8c5bafb
Temwani Msiska <138024035+temwani-msiska@users.noreply.github.com>
2025-03-31 09:01:29 +0200

    sitemap fixes

--- app/sitemap.xml/route.ts
+++ app/sitemap.xml/route.ts
@@ -1,28 +1,34 @@
-import { type MetadataRoute } from "next";
 
 export const dynamic = "force-static"; 
 
-export default function sitemap(): MetadataRoute.Sitemap {
-  return [
-    {
-      url: "https://temwanimsiska.dev",
-      lastModified: new Date().toISOString(),
-    },
-    {
-      url: "https://temwanimsiska.dev/#about",
-      lastModified: new Date().toISOString(),
-    },
-    {
-      url: "https://temwanimsiska.dev/#projects",
-      lastModified: new Date().toISOString(),
-    },
-    {
-      url: "https://temwanimsiska.dev/#blog",
-      lastModified: new Date().toISOString(),
-    },
-    {
-      url: "https://temwanimsiska.dev/#contact",
-      lastModified: new Date().toISOString(),
-    },
+export async function GET() {
+  const baseUrl = "https://temwanimsiska.dev";
+
+  const pages = [
+    "",
+    "#about",
+    "#projects",
+    "#blog",
+    "#contact",
   ];
+
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+  <urlset xmlns="https://www.sitemaps.org/schemas/sitemap/0.9">
+    ${pages
+      .map(
+        (page) => `
+      <url>
+        <loc>${baseUrl}/${page}</loc>
+        <lastmod>${new Date().toISOString()}</lastmod>
+      </url>
+    `
+      )
+      .join("")}
+  </urlset>`;
+
+  return new Response(sitemap, {
+    headers: {
+      "Content-Type": "application/xml",
+    },
+  });
 }
