74038e9cc05d6a074029a9691e9fcbd97b9d5a95
Temwani Msiska <138024035+temwani-msiska@users.noreply.github.com>
2025-03-31 08:59:36 +0200

    Speed Insights Implementation

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 // Load custom fonts
 const geistSans = Geist({
@@ -78,6 +79,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
