87d2f51596e901828f71a84ef5428b92217f6b55
Temwani Msiska <138024035+temwani-msiska@users.noreply.github.com>
2025-03-31 08:58:42 +0200

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { SpeedInsights } from "@vercel/speed-insights/next"
 
 // Load custom fonts
 const geistSans = Geist({
