03f25566e6da956aa8d36766aaf857ffd7fe6777
Temwani Msiska <138024035+temwani-msiska@users.noreply.github.com>
2025-04-16 15:27:30 +0200

    Email update

--- app/page.tsx
+++ app/page.tsx
@@ -140,7 +140,7 @@ export default function Home() {
                 variant="outline"
                 className="border-white text-white hover:bg-white hover:text-yellow-700 transition-colors duration-300"
                 onClick={() =>
-                  (window.location.href = "mailto:contact.temwanimsiska.dev")
+                  (window.location.href = "mailto:temwani.msiska@gmail.com")
                 }
               >
                 Email
